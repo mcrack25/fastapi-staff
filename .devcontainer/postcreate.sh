41dd26f8f67c26b0923f3e9a5511d29b116dd0a3
Ахмедов Мурад Алилович <amurad@cron.ooo>
2022-11-26 12:45:17 +0000

    Пробросить ssh ключи пользователя внутрь контейнера, для работы vscode с git в контейнере

---
+++ .devcontainer/postcreate.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+set -ex
+
+apt update && apt install -y curl postgresql-client && curl -sSL https://get.docker.com/ | sh && curl -L https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose && export EDITOR="code --wait" && chmod 400 /root/.ssh/id_rsa
